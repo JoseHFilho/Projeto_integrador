d982f13387fc3cc12fa47c61a2dde9d6890975b8
JoseHFilho <jhjose2567@hotmail.com>
2025-05-15 16:04:48 -0300

    Update and rename README.md to cadastro.py

---
+++ cadastro.py
@@ -0,0 +1,27 @@
+def cadastrar_usuario():
+    print("=== Cadastro de Usuário ===")
+    nome = input("Digite o seu nome: ")
+    email = input("Digite o seu e-mail: ")
+    cpf = input("Digite o seu CPF (somente números): ")
+    senha = input("Digite a sua senha: ")
+
+    print("\nCadastro realizado!")
+
+    return nome, email, cpf, senha
+
+
+def verificar_login(cpf_cadastrado, senha_cadastrada, nome):
+    print("\n=== Verificação de Cadastro ===")
+    vercpf = input("Digite novamente o seu CPF: ")
+    versenha = input("Digite novamente a sua senha: ")
+
+    if vercpf == cpf_cadastrado and versenha == senha_cadastrada:
+        print(f"\nBem-vindo(a), {nome}!")
+    else:
+        print("\nDados incorretos. Tente novamente!")
+
+
+# Execução principal
+if __name__ == "__main__":
+    nome, email, cpf, senha = cadastrar_usuario()
+    verificar_login(cpf, senha, nome)
